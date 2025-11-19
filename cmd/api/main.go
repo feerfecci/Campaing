d039c8f29f5733f7a35607870a101289d68907e5
fernandoVolpix <fernandofecci@hotmail.com>
2025-11-19 13:39:26 -0300

    air live

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,20 +35,23 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	service := campaign.Service{}
+	service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
 
 	r.Post("/campaign", func(w http.ResponseWriter, r *http.Request) {
 		var request contract.NewCampaign
-		err := render.DecodeJSON(r.Body, &request)
-
-		if err != nil {
-			println(err)
-		}
+		render.DecodeJSON(r.Body, &request)
 
 		id, err := service.Create(request)
 
 		if err != nil {
-			render.Status(r, 400) //status de criação
+
+			if errors.Is(err, internalerrors.ErrInternal) {
+				render.Status(r, 500)
+			} else {
+				render.Status(r, 400)
+			}
 			render.JSON(w, r, map[string]string{"error": err.Error()})
 			return
 		}
