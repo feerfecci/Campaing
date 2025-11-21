257bdcd0f393c09647b3105520fcb60c0f63220a
fernandoVolpix <fernandofecci@hotmail.com>
2025-11-21 10:42:18 -0300

    nem db

---
+++ internal/infrastruct/database/new_db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDb() *gorm.DB {
+	dsn := "host=localhost user=emailn_dev password=emailn123456 dbname=emailn port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("fail to connect to database")
+	}
+	return db
+
+}
